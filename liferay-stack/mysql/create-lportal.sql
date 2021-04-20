0763ba7acd7eeb7dbc842eb2ab62b3df3e000526
Christian Berndt <berndt@kulturtechniker.de>
2021-04-20 22:12:47 +0200

    Initial check-in of liferay-stack.

---
+++ liferay-stack/mysql/create-lportal.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS lportal;
+CREATE DATABASE lportal CHARACTER SET utf8;
+-- CREATE USER 'liferay'@'*' IDENTIFIED BY 'secret';
+-- GRANT ALL PRIVILEGES TO liferay;
+-- USE lportal;
